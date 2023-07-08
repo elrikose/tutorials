d0bccb353d4c634eb1d1f96a8932c3373924f99b
Erik Martin <emartin@softdocs.com>
2023-07-08 13:34:21 -0400

    [terraform] Add fix for the azurerm provider needing to be registered

--- terraform/azure-vm-linux/provider.tf
+++ terraform/azure-vm-linux/provider.tf
@@ -14,5 +14,7 @@ provider "azurerm" {
   tenant_id       = var.azure_tenant_id
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
+
+  skip_provider_registration = "true"
   features {}
 }
